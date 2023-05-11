60ae935815fb02de13659934011c73b006e7968a
Agustin Pagliuca <116588435+AgustinPagliuca@users.noreply.github.com>
2023-05-10 23:58:44 -0300

    Add files via upload

---
+++ PrimerPreEntrega1Pagliuca/app.js
@@ -0,0 +1,18 @@
+let objeto = 2000
+
+alert("Quisieras comprar esta remera por tan solo $2000?")
+
+let pregunta = Number(prompt("Ingrese el monto exacto"))
+
+if(objeto === pregunta){
+    alert("Compra exitosa")
+} else {
+    alert("Compra rechazada")
+}
+
+let unidades = 4 
+
+while (unidades >= 4) {
+    alert(i)
+    i++
+}
